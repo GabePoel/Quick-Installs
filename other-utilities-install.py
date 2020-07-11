74b4e04e12e5165cc24a181a42586dcd215cfe2d
Gabriel Perko-Engel <gabriel@pop-os.localdomain>
2020-07-11 15:35:15 -0700

    Started utilities installed.

---
+++ other-utilities-install.py
@@ -0,0 +1,4 @@
+import util
+
+util.make_launcher('audible.desktop', 'Audible', 'firefox audible.com', "Amazon's audiobook service.", 'audible', categories=['Audio'])
+# util.make_launcher('messenger.desktop', 'Messenger', 'firefox messenger.com', "Facebook instant messaging.", 'google-sheets', categories=['Office'])
