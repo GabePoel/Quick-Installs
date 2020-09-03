9eddbd6a05f2cb3f51c7ef8dd6681b24060fc135
GabePoel <gabrielpoel@gmail.com>
2020-09-03 16:40:54 -0700

    Thu 03 Sep 2020 04:36:28 PM PDT

--- microsoft-office-install.py
+++ microsoft-office-install.py
@@ -1,9 +1,10 @@
 import util
+import elec
 
-util.make_launcher('microsoft-word.desktop', 'Microsoft Word', 'xdg-open https://office.com/launch/word', "Microsoft's document writer.", 'ms-word', categories=['Office'])
-util.make_launcher('microsoft-excel.desktop', 'Microsoft Excel', 'xdg-open https://office.com/launch/excel', "Microsoft's spreadsheet editor.", 'ms-excel', categories=['Office'])
-util.make_launcher('microsoft-powerpoint.desktop', 'Microsoft PowerPoint', 'xdg-open https://office.com/launch/powerpoint', "Microsoft's presentation maker.", 'ms-powerpoint', categories=['Office'])
-util.make_launcher('microsoft-onenote.desktop', 'Microsoft OneNote', 'xdg-open https://onenote.com', "Microsoft's note taker.", 'ms-onenote', categories=['Office'])
-util.make_launcher('microsoft-onedrive.desktop', 'Microsoft OneDrive', 'xdg-open https://onedrive.live.com', "Microsoft's cloud drive.", 'ms-onedrive', categories=['Office'])
-util.make_launcher('microsoft-outlook.desktop', 'Microsoft Outlook', 'xdg-open https://outlook.live.com', "Microsoft's email portal.", 'ms-outlook', categories=['Office'])
-util.make_launcher('microsoft-office.desktop', 'Microsoft Office', 'xdg-open https://office.com', "Microsoft's complete office suite.", 'ms-office', categories=['Office'])
+elec.electron_launcher('microsoft-excel.desktop', 'Microsoft Excel', 'https://office.com/launch/excel', 'ms-excel', "Microsoft's spreadsheet editor.", categories=['Office'])
+elec.electron_launcher('microsoft-powerpoint.desktop', 'Microsoft PowerPoint', 'https://office.com/launch/powerpoint', 'ms-powerpoint', "Microsoft's presentation maker.", categories=['Office'])
+elec.electron_launcher('microsoft-onenote.desktop', 'Microsoft OneNote', 'https://onenote.com', 'ms-onenote', "Microsoft's note taker.", categories=['Office'])
+elec.electron_launcher('microsoft-onedrive.desktop', 'Microsoft OneDrive', 'https://onedrive.live.com', 'ms-onedrive', "Microsoft's cloud drive.", categories=['Office'])
+elec.electron_launcher('microsoft-outlook.desktop', 'Microsoft Outlook', 'https://outlook.live.com', 'ms-outlook', "Microsoft's email portal.", categories=['Office'])
+elec.electron_launcher('microsoft-office.desktop', 'Microsoft Office', 'https://office.com', 'ms-office', "Microsoft's complete office suite.", categories=['Office'])
+elec.electron_launcher('microsoft-word.desktop', 'Microsoft Word', 'https://office.com/launch/word', 'ms-word', "Microsoft's document writer.", categories=['Office'])
